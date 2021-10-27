578a42a47f1701b76b48a31756d0ed0b3df22d26
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-10-26 23:10:07 -0500

    cards are configured in the main view

--- begin/Gallery/Controller/GalleryController.swift
+++ begin/Gallery/Controller/GalleryController.swift
@@ -62,18 +62,47 @@ class GalleryController: UIViewController {
   
   // MARK: - Setting Views
   private func setupViews() {
-    view.addSubviews(cardView1)       //Se agregan tarjetas a la vista.
+    view.addSubviews(cardView1, cardView2, cardView3, cardView4) //Se agregan                                                               tarjetas a la vista.
   }
   
   // MARK: - Setting Constraints
   private func setupConstraints() {
+    let safeArea = view.safeAreaLayoutGuide
+    let viewFrame = view.bounds
+    //card 1
     NSLayoutConstraint.activate([
-      //2
-      cardView1.centerXAnchor.constraint(equalTo: view.centerXAnchor), //Centro del frame
-      cardView1.centerYAnchor.constraint(equalTo: view.centerYAnchor),  //Centro del frame
-      //3
-      cardView1.widthAnchor.constraint(equalToConstant: 120),
-      cardView1.heightAnchor.constraint(equalToConstant: 200)
+      cardView1.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      cardView1.topAnchor.constraint(equalTo: safeArea.topAnchor),
+      cardView1.widthAnchor.constraint(equalToConstant: viewFrame.width/2),
+      cardView1.heightAnchor.constraint(equalToConstant: viewFrame.height/2)
     ])
+        
+    //card 2
+    NSLayoutConstraint.activate([
+      cardView2.leadingAnchor.constraint(equalTo: cardView1.trailingAnchor),
+      cardView2.topAnchor.constraint(equalTo: safeArea.topAnchor),
+      cardView2.widthAnchor.constraint(equalToConstant: viewFrame.width/2),
+      cardView2.heightAnchor.constraint(equalToConstant: viewFrame.height/2),
+      cardView2.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
+        
+    //card 3
+    NSLayoutConstraint.activate([
+      cardView3.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      cardView3.topAnchor.constraint(equalTo: cardView1.bottomAnchor),
+      cardView3.widthAnchor.constraint(equalToConstant: viewFrame.width/2),
+      cardView3.heightAnchor.constraint(equalToConstant: viewFrame.height/2),
+      cardView3.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+    ])
+        
+    //card 4
+    NSLayoutConstraint.activate([
+      cardView4.leadingAnchor.constraint(equalTo: cardView3.trailingAnchor),
+      cardView4.topAnchor.constraint(equalTo: cardView2.bottomAnchor),
+      cardView4.widthAnchor.constraint(equalToConstant: viewFrame.width/2),
+      cardView4.heightAnchor.constraint(equalToConstant: viewFrame.height/2),
+      cardView4.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+    ])
+
   }
 }
